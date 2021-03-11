3479bee1ce15db524ed96f1081d90ecaf36fed83
Hector Herrera <hectorsum81@hotmail.com>
2021-03-11 10:10:59 -0500

    UI/UX done for our game

---
+++ src/Space.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let space = "";
+</script>
+<style>
+  .space{
+    border: solid 1px grey;
+    height: 125px;
+    width: 125px;
+    flex:1;
+    align-content: center;
+    justify-content: center;
+    font-size: 75px;
+    text-align: center;
+    line-height: 125px;
+    cursor: pointer;
+  }
+</style>
+
+<div class="space">{space}</div>
