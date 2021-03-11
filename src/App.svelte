7249b04b03562f12e1af6f9f2f62d05ad11124f2
Hector Herrera <hectorsum81@hotmail.com>
2021-03-11 11:26:23 -0500

    connection between front and back done

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,61 @@
 <script>
-  import Space from './Space.svelte'
+  import Space from './Space.svelte';
+  import gameStore from './game-store';
+  let board = ['', '', '', '', '', '', '', '', '', '', ''];
+  let nextPlayer = '';
+  let winner;
+  let numberOfPeeps = 0;
+  gameStore.subscribe((data) => {
+    if (!data) {
+      return;
+    }
+    winner = data.winner;
+    nextPlayer = data.nextPlayer;
+    board = data.board;
+    numberOfPeeps = data.numberOfPeeps;
+  });
 </script>
 
+<main>
+  <h1>Tic Tac Toe</h1>
+  <h2>Number of people playing: {numberOfPeeps}</h2>
+  {#if winner == 'TIE'}
+    <h2>Tie Game!!</h2>
+  {:else}
+    <h2>Player {winner} won!!</h2>
+  {/if}
+  <h2>Player X</h2>
+  <div class="row">
+    <Space space={board[0]} />
+    <Space space={board[1]} />
+    <Space space={board[2]} />
+  </div>
+  <div class="row">
+    <Space space={board[3]} />
+    <Space space={board[4]} />
+    <Space space={board[5]} />
+  </div>
+  <div class="row">
+    <Space space={board[6]} />
+    <Space space={board[7]} />
+    <Space space={board[8]} />
+  </div>
+  {#if winner}
+    <button> New Game </button>
+  {/if}
+</main>
+
 <style>
-  main{
+  main {
     width: 475px;
     margin: 0 auto;
     border: black solid;
     height: 1000px;
   }
-  .row{
-    display:flex;
+  .row {
+    display: flex;
   }
-  button{
+  button {
     width: 100%;
     margin-top: 20px;
     border-radius: 0;
@@ -22,31 +65,7 @@
     border: none;
     outline: none;
   }
-  button:hover{
+  button:hover {
     outline: none;
   }
 </style>
-
-<main>
-  <h1>Tic Tac Toe</h1>
-  <h2>Number of people playing:</h2>
-  <h2>Player X</h2>
-  <div class="row">
-    <Space space="X"/>
-    <Space space="X"/>
-    <Space space="X"/>
-  </div>
-  <div class="row">
-    <Space space="X"/>
-    <Space space="X"/>
-    <Space space="X"/>
-  </div>
-  <div class="row">
-    <Space space="X"/>
-    <Space space="X"/>
-    <Space space="X"/>
-  </div>
-  <button>
-    New Game
-  </button>
-</main>
